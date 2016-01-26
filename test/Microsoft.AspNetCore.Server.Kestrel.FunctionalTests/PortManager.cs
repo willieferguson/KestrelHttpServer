8a46b1e5f538c4b0e825c1e83e90747fcb893d35
Cesar Blum Silveira <cesars@microsoft.com>
2016-01-25 17:49:54 -0800

    Fix test/Microsoft.AspNetCore.Server.KestrelTests/project.json.

--- test/Microsoft.AspNetCore.Server.Kestrel.FunctionalTests/PortManager.cs
+++ test/Microsoft.AspNetCore.Server.Kestrel.FunctionalTests/PortManager.cs
@@ -3,7 +3,7 @@
 
 using System.Threading;
 
-namespace Microsoft.AspNet.Server.Kestrel.FunctionalTests
+namespace Microsoft.AspNetCore.Server.Kestrel.FunctionalTests
 {
     public static class PortManager
     {
